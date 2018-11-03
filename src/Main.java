f60eeff1f6f1d38c3c5c593abff084bb1f0cba19
Karen <bezimeninofamily@gmail.com>
2018-11-03 21:03:56 +0100

    chapter 15 socket

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,11 @@ public class Main {
     public static void main(String[] args) {
 
         // Chapter 14
-        ReadAFile_C14 readAFile_c14 = new ReadAFile_C14();
-        readAFile_c14.go();
+        // ReadAFile_C14 readAFile_c14 = new ReadAFile_C14();
+        // readAFile_c14.go();
 
         // Chapter 15
-
+        DailyAdviceClient client = new DailyAdviceClient();
+        client.go();
     }
 }
