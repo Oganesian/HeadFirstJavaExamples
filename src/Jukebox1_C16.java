26ed3ad844523f71e05981c4a91cd1a2b7050b84
Karen <bezimeninofamily@gmail.com>
2019-01-12 16:57:45 +0100

    ADD Jukebox1.class
    CHAPTER 16

---
+++ src/Jukebox1_C16.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Jukebox1_C16 {
+    ArrayList<String> songList = new ArrayList<String>();
+
+    public void go() {
+        getSongs();
+        System.out.println(songList);
+        Collections.sort(songList);
+        System.out.println(songList);
+    }
+
+    void getSongs() {
+        try {
+            File file = new File("SongList_C16.txt");
+            BufferedReader reader = new BufferedReader(new FileReader(file));
+            String line = null;
+            while((line = reader.readLine()) != null) {
+                addSong(line);
+            }
+        } catch (Exception ex) {ex.printStackTrace();}
+    }
+
+    void addSong(String lineToParse) {
+        String[] tokens = lineToParse.split("/");
+        songList.add(tokens[0]);
+    }
+}
