39b54ebd58f3bb8be2c9609f8ccddf7c263e3055
Karen <bezimeninofamily@gmail.com>
2019-01-12 16:58:04 +0100

    use Jukebox1.class in main

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,20 @@
 public class Main {
     public static void main(String[] args) {
 
-        // Chapter 14
+        // CHAPTER 14
+
         // ReadAFile_C14 readAFile_c14 = new ReadAFile_C14();
         // readAFile_c14.go();
 
-        // Chapter 15
+        // CHAPTER 15
+
         // DailyAdviceServer_C15 server = new DailyAdviceServer_C15();
         // server.go();
         // DailyAdviceClient_С15 client = new DailyAdviceClient_С15();
         // client.go();
-        new SimpleChatClientA_C15().go();
+        // new SimpleChatClientA_C15().go();
+
+        // CHAPTER 16
+        new Jukebox1_C16().go();
     }
 }
