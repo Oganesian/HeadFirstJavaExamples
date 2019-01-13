701b7d280ca9fe670a810470bd61f4437d7fb003
Karen <bezimeninofamily@gmail.com>
2019-01-13 13:52:13 +0100

    same with generics
    <? extends Object>

--- src/CHAPTER_16/TestGenerics.java
+++ src/CHAPTER_16/TestGenerics.java
@@ -1,14 +1,23 @@
 package CHAPTER_16;
 
+import java.util.ArrayList;
+
 public class TestGenerics {
     public void go() {
-        Animal[] animals = {new Dog(), new Cat(), new Dog()};
-        Dog[] dogs = {new Dog(), new Dog(), new Dog()};
+        ArrayList<Animal> animals = new ArrayList<Animal>();
+        animals.add(new Dog());
+        animals.add(new Cat());
+        animals.add(new Dog());
+
         takeAnimals(animals);
+
+        ArrayList<Dog> dogs = new ArrayList<Dog>();
+        dogs.add(new Dog());
+        dogs.add(new Dog());
         takeAnimals(dogs);
     }
 
-    public void takeAnimals(Animal[] animals) {
+    public void takeAnimals(ArrayList<? extends Animal> animals) {
         for(Animal a: animals) {
             a.eat();
         }
